c76d8d526a35a687f02e76acaf2bd2cdd3658784
Karsli Hilal (LWN) <lwnkah7@lwn.liebherr.i>
2021-09-16 16:02:35 +0200

    changes

--- Aufgabe Bankautomat/Aufgabe Bankautomat.cpp
+++ Aufgabe Bankautomat/Aufgabe Bankautomat.cpp
@@ -10,13 +10,15 @@ int main()
 	system("chcp 1252");
 	printf("\n\r");
 
-	int kontonummern[10];
-	int kontostand = 0;
+	/*int kontonummern[10];*/
+	
+	int kontonummern[]; {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
 
 	int eingabe;
-	int E; //Einzahlen
-	int A; //Abheben
-	int Z; //Zurück
+	//Einzahlen - E
+	//Abheben - A
+	//Zurück - Z
 
 	int einzahlungsbetrag;
 	int abhebungsbetrag;
@@ -27,11 +29,11 @@ int main()
 	printf("\nKontonummer: ");
 	scanf("%i", &kontonummern);
 
-	printf("\n\rKontostand: \n\r", &kontostand);
+	printf("\n\rKontostand: \n\r", &kontonummern);
 	
-	printf("Einzahlen: E\n\r", &E);
-	printf("Abheben: A\n\r", &A);
-	printf("Zurück: Z\n\r", &Z);
+	printf("Einzahlen: E\n\r");
+	printf("Abheben: A\n\r");
+	printf("Zurück: Z\n\r");
 	printf("Eingabe: \n\r", &eingabe);
 	scanf("\n", &eingabe);
 
@@ -39,7 +41,7 @@ int main()
 
 	for (unsigned int i = 0; i < eingabe; i++)
 	{
-	if (eingabe == E)
+	if (eingabe )
 	{
 		printf("\n\Wie viel Geld möchten Sie einzahlen?: ", &einzahlungsbetrag); 
 		scanf("\n\r%i", &einzahlungsbetrag);
