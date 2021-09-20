77bce7280f920a1c8c762b52f410499fe40a654b
Karsli Hilal (LWN) <lwnkah7@lwn.liebherr.i>
2021-09-20 16:03:20 +0200

    added aufgabenstellung

--- Aufgabe Bankautomat/Aufgabe Bankautomat.cpp
+++ Aufgabe Bankautomat/Aufgabe Bankautomat.cpp
@@ -56,24 +56,8 @@ int main()
 		printf("\n\rNeuer Saldo nach Einzahlung: %i", (kontostand + einzahlung));
 
 	}
-	//auto int acc_no;
-	//float add_money;
-
-	//printf("Enter account number you want to deposit money:");
-	//scanf("%d", &acc_no);
-	//printf("\nThe current balance for account %d is %f \n",
-	//	acc_no, account[acc_no - 1].available_balance);
-	//printf("\nEnter money you want to deposit :  ");
-	//scanf("%f", &add_money);
-
-	//while (acc_no = account[acc_no - 1].acc_number)
-	//{
-	//	account[acc_no - 1].available_balance =
-	//		account[acc_no - 1].available_balance + add_money;
-	//	printf("\nThe New balance for account %d is %f \n",
-	//		acc_no, account[acc_no - 1].available_balance);
-	//	break;
-	//}acc_no++;
+ 
+	// Switch und While adden, damit die Schleife unendlich  mal wiederholt wird, bis man es abbricht!
 
 
 	if (eingabe == 'A')
